c8258864a55deedae63bff2ad0022a0e7e13e479
Christopher Mühl <christopher@muehl.dev>
2025-08-17 13:13:50 +0200

    chore: migrate from flake to devenv

---
+++ devenv.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  env.PROTOC = "${pkgs.protobuf}/bin/protoc";
+  env.PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+
+  packages = with pkgs; [
+    git
+    protobuf
+  ];
+
+  languages.rust.enable = true;
+  languages.javascript.bun.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    clippy.enable = true;
+    alejandra.enable = true;
+  };
+}
